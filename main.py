2a48da60ec64ce85c0eb3cb634e7683a9f39bc57
roman <rpgdkn@gmail.com>
2023-01-03 00:36:30 +0300

    8.5

--- main.py
+++ main.py
@@ -6,9 +6,20 @@
 
 # Напишите программу для вывода общего количества уникальных символов во всех считанных словах без учета регистра.
 
-x = int(input())
-new_set=set()
-for i in range(x):
-    for j in (input().lower()):
-        new_set.add(j)
-print(len(new_set))
+# x = int(input())
+# new_set=set()
+# for i in range(x):
+#     for j in (input().lower()):
+#         new_set.add(j)
+# print(len(new_set))
+
+# Напишите программу для определения общего количества различных слов в строке текста.
+
+# s = input().lower().split()
+# print(s)
+# for i in range (len(s)):
+#     s[i]=s[i].strip('.,;:-?!')
+# print(s)
+# set_words= set(s)
+# print(s)
+# print(len(set_words))
