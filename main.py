10c3235665a3c050210b68520127235bcdab4ed0
roman <rpgdkn@gmail.com>
2023-01-06 21:20:50 +0300

    8.5

--- main.py
+++ main.py
@@ -182,3 +182,6 @@
 # чтобы получить множество, содержащее первую букву каждого слова (в нижнем регистре) списка words.
 # Результат вывести на одной строке в алфавитном порядке, разделяя элементы одним символом пробела.
 
+# words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
+# set = set(i[0].lower() for i in words)
+# print(*sorted(set))
