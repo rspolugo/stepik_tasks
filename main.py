45e652a1975529f3d92b42084273998da9ba9e7b
roman <rpgdkn@gmail.com>
2023-01-14 21:34:49 +0300

    8.5

--- main.py
+++ main.py
@@ -521,3 +521,9 @@
 # favorite_numbers = {'timur': 17, 'ruslan': 7, 'larisa': 19, 'roman': 123, 'rebecca': 293, 'ronald': 76, 'dorothy': 62, 'harold': 36, 'matt': 314, 'kim': 451, 'rosaly': 18, 'rustam': 89, 'soltan': 111, 'amir': 654, 'dima': 390, 'amiran': 777, 'geor': 999, 'sveta': 75, 'rita': 909, 'kirill': 404, 'olga': 271, 'anna': 55, 'madlen': 876}
 # result = {v: favorite_numbers[v] for v in favorite_numbers if len(str(favorite_numbers[v]))==2}
 # print(result)
+
+# Дополните приведенный код, используя генератор, так, чтобы получить словарь result, состоящий из всех элементов словаря months , в которых ключ и значение поменялись местами.
+
+# months = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+# result = {v: k for k, v in months.items()}
+# print(result)
