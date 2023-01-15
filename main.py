2c445a153ce491601672191e22222e2279a29d91
roman <rpgdkn@gmail.com>
2023-01-15 19:09:01 +0300

    8.5

--- main.py
+++ main.py
@@ -563,8 +563,6 @@
 # а значением – список соответствующих кодов ASCII символов данной строки
 
 # words = ['hello', 'bye', 'yes', 'no', 'python', 'apple', 'maybe', 'stepik', 'beegeek']
-#
-#
 # result = {k:[ord(i) for i in k] for k in words}
 # print(result)
 
@@ -576,3 +574,11 @@
 # result = {k:v for k,v in letters.items() if k not in remove_keys}
 # print(result)
 
+# В переменной students хранится словарь, содержащий информацию о росте (в см) и весе (в кг) студентов.
+# Дополните приведенный код, используя генератор, чтобы получить словарь result, состоящий из всех элементов словаря students, где указан рост больше
+# 167 см, а вес меньше 75 кг.
+
+students = {'Timur': (170, 75), 'Ruslan': (180, 105), 'Soltan': (192, 68), 'Roman': (175, 70), 'Madlen': (160, 50), 'Stefani': (165, 70), 'Tom': (190, 90), 'Jerry': (180, 87), 'Anna': (172, 67), 'Scott': (168, 78), 'John': (186, 79), 'Alex': (195, 120), 'Max': (200, 110), 'Barak': (180, 89), 'Donald': (170, 80), 'Rustam': (186, 100), 'Alice': (159, 59), 'Rita': (170, 80), 'Mary': (175, 69), 'Jane': (190, 80)}
+
+result = {k:v for k,v in students.items() if v[0]>167 and v[1]<75}
+print(result)
